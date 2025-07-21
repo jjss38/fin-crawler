3cc828926d7b8b7a1f4492a3239e170494f79f5f
jjss38 <ukino777@gmail.com>
2025-07-22 01:45:09 +0900

    Delete fin-crawler directory

--- fin-crawler/pages/api/crawled-data.js
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  res.status(200).json([
-    { address: "서울시 강남구 역삼동", area: "18평", price: "5.3억", type: "매매", isUrgent: true },
-    { address: "서울시 성동구 성수동", area: "12평", price: "2.1억", type: "전세", isUrgent: false },
-    { address: "부산시 해운대구 우동", area: "10평", price: "75만원", type: "월세", isUrgent: true }
-  ]);
-}
--- fin-crawler/pages/api/download-excel.js
+++
@@ -1,6 +0,0 @@
-
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-  res.setHeader('Content-Disposition', 'attachment; filename="real_estate_data.xlsx"');
-  res.end('This would be binary excel content.');
-}
